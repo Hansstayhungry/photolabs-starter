9f5c774066344e7825d9b2597896906986aa22aa
Hans Wang <hansmikaa@live.cn>
2023-07-17 03:29:46 -0400

    use homeroute and send to app for render

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigation from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 
-const HomeRoute = () => 
+const HomeRoute = () => (
   <div className="home-route">
-    {/* Insert React */}
+    <TopNavigation/>
+    <PhotoList/>
   </div>
+)
 
 export default HomeRoute;
